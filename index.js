01365a92bc3740d093aa617bd8f3603632114829
MauricioGiaconia <mauriciogiaco@gmail.com>
2023-06-15 20:03:03 -0300

    Changed port of the server: 3001 to 3000

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const server = require('./src/api/app.js');
 const { conn } = require('./src/api/database/db.js');
 
+/**
+ * Función que se encargará de iniciar el servidor en el puerto 3000
+ */
 function startServer() {
 
-  server.listen('3001', () => {
-      console.log('Escuchando en el puerto 3001');
+  server.listen('3000', () => {
+      console.log('Escuchando en el puerto 3000');
     });
  
 }
